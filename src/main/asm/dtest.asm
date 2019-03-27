ca62023496c3439bbde2c2c01e9dbd2aa296ade9
doenn <none@none.com>
2019-03-27 08:25:54 -0400

    2010

---
+++ src/main/asm/dtest.asm
@@ -0,0 +1,37 @@
+include 'format/format.inc'
+
+
+format MS64 COFF
+
+include 'Exports.inc'
+
+include 'dtest.inc'
+
+
+namespace dtest
+dtDelta dtest.subspace.subsubspace.subsubsubspace,2
+end namespace
+
+public start as 'start'
+
+section '.data' data readable writeable align 16
+
+	val7h db 0
+
+
+section '.text' code readable writeable executable align 16
+
+
+;align 16
+start:
+       push rbp
+       mov rbp, rsp
+       and rsp, -32
+       sub rsp, 8*12;8;7;8;9
+       and rsp, -32
+
+
+	 mov rcx, 0
+	 sub rsp, 8*4
+	 call [ExitProcess]
+	 add rsp, 8*4
---
+++ src/main/asm/make_2010.cmd
@@ -0,0 +1,5 @@
+set include=include
+
+fasmg dtest.asm dtest.obj
+
+.\Build_2010.bat
